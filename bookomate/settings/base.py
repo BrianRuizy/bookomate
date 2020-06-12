8622e8042713ba3cea11d628be5433343d1a95e2
root <brianruiz123@yahoo.com>
2020-06-12 02:46:08 -0500

    login and logout functionality

--- bookomate/settings/base.py
+++ bookomate/settings/base.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'bookomate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect home
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
