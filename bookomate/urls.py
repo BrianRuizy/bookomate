f5c03d4a639ae3e81b2e0f3407630a58c99da42d
root <brianruiz123@yahoo.com>
2020-06-12 03:04:01 -0500

    user account signup functionality

--- bookomate/urls.py
+++ bookomate/urls.py
@@ -19,6 +19,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
