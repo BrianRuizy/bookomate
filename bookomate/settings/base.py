10eff1804f3c7070abefbfb8ac7f6838d669c3a0
Brian <brianruiz123@yahoo.com>
2020-07-07 03:29:25 -0500

    heroku configs

--- bookomate/settings/base.py
+++ bookomate/settings/base.py
@@ -100,6 +100,9 @@ DATABASES = {
     )
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
