c5c1a6d84c109dc548d356d00c43b7c749ab1c60
STM9000050 <9000050@student.munster.us>
2020-07-18 14:42:49 -0500

    Do not go on master

--- bookomate/settings/base.py
+++ bookomate/settings/base.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 6610
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookomate.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookomate.herokuapp.com', 'bookomatetest.herokuapp.com']
 
 
 # Application definition
