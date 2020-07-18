75df2af264958bd781ae68d04db241b6484551ff
STM9000050 <9000050@student.munster.us>
2020-07-18 15:29:29 -0500

    fix

--- bookomate/settings/base.py
+++ bookomate/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 6610
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookomate.herokuapp.com', 'bookomatetest.herokuapp.com']
 
