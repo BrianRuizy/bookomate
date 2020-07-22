b7afc8eccdb6b6cde09242e954dd8b51d9fb24be
Jeremy Chrimes <jeremy.chrimes@gmail.com>
2020-07-23 09:55:00 +1000

    Changes to database model

--- bookomate/calendarium/models.py
+++ bookomate/calendarium/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 from django.contrib.auth.models import AnonymousUser
 
 class Event(models.Model):
-    event_name = models.TextField("Event Name", help_text = "The tile of the event")
+    event_name = models.TextField("Event Name", help_text = "The tile of the event", default="")
     event_location = models.TextField("Event Location", help_text = "The location of the event", default="")
     start_date = models.DateField('Event date', help_text='The date the event will be on', default=timezone.now)
     end_date = models.DateField('Event date', help_text='The date the event will be on', default=start_date)
     start_time = models.TimeField('Starting time', help_text='The time the event will start')
     end_time = models.TimeField('Final time', help_text='The time the event will end')
-    _user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, on_delete=models.CASCADE)
+    _user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, on_delete=models.CASCADE)
