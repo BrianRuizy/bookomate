6f9727da917f7a67af90b9c7467160a1f66f5760
Jeremy Chrimes <jeremy.chrimes@gmail.com>
2020-07-22 17:46:53 +1000

    Fixed relationship with user

--- bookomate/calendarium/models.py
+++ bookomate/calendarium/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils import timezone
+from django.contrib.auth.models import AnonymousUser
 
 class Event(models.Model):
     event_name = models.TextField("Event Name", help_text = "The tile of the event")
-    event_location = models.TextField("Event Location", help_text = "The location of the event")
-    start_day = models.DateField('Event date', help_text='The date the event will be on')
-    end_day = models.DateField('Event date', help_text='The date the event will be on', default = start_day)
+    event_location = models.TextField("Event Location", help_text = "The location of the event", default="")
+    start_date = models.DateField('Event date', help_text='The date the event will be on', default=timezone.now)
+    end_date = models.DateField('Event date', help_text='The date the event will be on', default=start_date)
     start_time = models.TimeField('Starting time', help_text='The time the event will start')
     end_time = models.TimeField('Final time', help_text='The time the event will end')
-    _user = models.ForeignKey(User, on_delete = models.CASCADE)
+    _user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, on_delete=models.CASCADE)
